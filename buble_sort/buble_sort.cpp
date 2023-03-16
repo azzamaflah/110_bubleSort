97fd532405dfa7bef5a391938993c29fc9f83c4b
azzamaflah <azzamhebat46@gmail.com>
2023-03-16 16:10:22 +0700

    void display

--- buble_sort/buble_sort.cpp
+++ buble_sort/buble_sort.cpp
@@ -54,6 +54,17 @@ void unsorted() {
     cout << endl;                                           // Output baris kosong
 }
 
+void display() {                                            // procedur untuk menampilkan hasil
+    cout << endl;                                           // output baris kosong
+    cout << "===================" << endl;                  // output ke layar
+    cout << "Element Array yang telah tersusun" << endl;    // output ke layar
+    cout << "===================" << endl;                  // output ke layar
+    for (int j = 0; j < n; j++) {                           // looping dengan j dimulai dari 0 hingga n-1
+        cout << a[j] << endl;                               // output ke layar
+    }
+    cout << endl;                                           // output baris kosong
+}
+
 int main()
 {
     std::cout << "Hello World!\n";
